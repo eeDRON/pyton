95d3d30f54907a9bdfd0e1aac6da968691f308e8
eeDRON <mr1tv1616@gmail.com>
2025-07-09 14:45:11 +0200

    Add files via upload

---
+++ array.py
@@ -0,0 +1,7 @@
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia"]
+
+states_of_america[1] = "Pencilvenia"
+
+states_of_america.extend(["Angelaland", "Jack Bauer Land"])
+
+print(states_of_america)
---
+++ score.py
@@ -0,0 +1,16 @@
+student_score = [150, 142, 185, 120, 171, 184, 149, 24, 59, 64, 199,78, 65, 89, 86, 55, 91, 64, 89]
+
+max_score = 0
+for score in student_score:
+    if score > max_score:
+        max_score = score
+
+print(max_score)
+
+
+
+#sum = 0
+#for score in student_score:
+#    sum += score
+
+#print(sum)
